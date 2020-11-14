d61c83d1ea216a3a961471adc9ec56bfb7f43444
Jean-Luc Beuchat <jean-luc.beuchat@hevs.ch>
2020-11-14 21:15:25 +0100

    Dev environment

--- .devcontainer/install_dev_tools.bash
+++ .devcontainer/install_dev_tools.bash
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-# go install \
-#     github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway \
-#     github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2 \
-#     google.golang.org/protobuf/cmd/protoc-gen-go \
-#     google.golang.org/grpc/cmd/protoc-gen-go-grpc
+apt-get install -y nodejs npm vim
 
-apt-get install -y vim 
-#software-properties-common protobuf-compiler
+npm install --global yarn
+
+echo '"\e[A": history-search-backward
+"\e[B": history-search-forward
+set show-all-if-ambiguous on
+set completion-ignore-case on' > /root/.inputrc
+
+echo "export VAULT_ADDR=http://127.0.0.1:8200" >> /root/.bashrc
